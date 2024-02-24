641cb50b16d80bd6b3318a7c8a1efa1e0cbe7521
naveedsapi <naveedkhansapi409@gmail.com>
2024-02-24 18:48:06 +0100

    Sum added

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ var sum = a + b
 
 console.log('Sum : ' + sum);
 console.log(`Sum : ${sum}`);
+
+var a = 10
+var b = 20
+
+var sum = a + b
+
+console.log('Sum : ' + sum);
+console.log(`Sum : ${sum}`);
