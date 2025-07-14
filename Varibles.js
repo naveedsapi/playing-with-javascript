22d14810685f1be2c415b4f6d982a58cd67d4fcd
Sapi <naveedkhansapi@gmail.com>
2025-07-14 10:58:31 +0200

    Some Other Emple of Varible Added Into Project

--- Varibles.js
+++ Varibles.js
@@ -9,8 +9,8 @@ console.log("Name ist:"+x,"and Age :"+y);
 console.log("  i like pizza");
 
 //This is alert message in javaScript
- window.alert("This is in Alert");
-window.alert("this is in alert");
+// window.alert("This is in Alert");
+//window.alert("this is in alert");
 
 //This is Simple Element geting by id
 document.getElementById("hid").textContent="this is Backerei Kettler";
@@ -18,3 +18,12 @@ document.getElementById("pid").textContent="I Like Pizza";
 
 document.getElementById("pid2").textContent="This is The Second Paragraph";
 document.getElementById("hid2").textContent="This is The Head 2 ";
+
+//this is another Example of Varible
+let Fullname="Sapi";
+let age=28;
+let istStudent=false;
+
+document.getElementById("lab1").textContent="Your FullName Is :"+Fullname;
+document.getElementById("lab2").textContent="Your Age Is: "+age;
+document.getElementById("lab3").textContent="You are a Student: "+istStudent;
