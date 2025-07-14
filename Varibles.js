e97f0174ad8382131f3def4d1a5e3c608b20d350
Sapi <naveedkhansapi@gmail.com>
2025-07-14 06:30:22 +0200

    Some change added into Varieble page

--- Varibles.js
+++ Varibles.js
@@ -1,6 +1,17 @@
 
+ // this is in javascript comment
 let x="Sapi";
 let y=20;
 
-console.log("Name ist:"+x,"and Age :"+y);
 
+// This is simple message in javascript
+console.log("Name ist:"+x,"and Age :"+y); 
+console.log("  i like pizza");
+
+//This is alert message in javaScript
+ window.alert("This is in Alert");
+window.alert("this is in alert");
+
+//This is Simple Element geting by id
+document.getElementById("hid").textContent="this is Backerei Kettler";
+document.getElementById("pid").textContent="I Like Pizza";
