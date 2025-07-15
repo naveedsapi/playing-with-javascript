50b31b4e0dff5e9d74ecdda587bcbbedf4457621
Sapi <naveedkhansapi@gmail.com>
2025-07-15 11:55:26 +0200

    Const Has been added into page

--- UserInput.js
+++ UserInput.js
@@ -24,12 +24,14 @@ document.getElementById("btnsubmit").onclick = function(){
 //                         (strings, number, booleans)
 // 1 .Example
 
+/*
 let age=window.prompt("how old are you");
 age=Number(age);
 age +=1;
 console.log(age);
 console.log(age, typeof age);
 
+// 2 Example type converston
 let x = "";
 let y = "";
 let z = "";
@@ -41,4 +43,33 @@ z = Boolean (z);
 console.log(x, typeof x);
 console.log(y, typeof y);
 console.log(z, typeof z);
+*/
+// const = a variable that can't be changed
+// 1 Example 
+//let PI =3.1415;
+//let radius;
+//let circumference;
 
+//radius = window.prompt("Enter the Radius of a circle");
+//radius = Number (radius);
+//circumference = 2 * PI * radius;
+//document.getElementById("PiCount").textContent = circumference;
+//console.log(circumference);
+
+// 2 Example
+let PI =3.1415;
+let radius;
+let circumference;
+
+document.getElementById("btnpi") . onclick = function(){
+  radius  =  document.getElementById("txtpi"). value;
+  radius = Number (radius);
+  circumference = 2 * PI * radius;
+  document.getElementById("PiCount").textContent = circumference;
+}
+
+//radius = window.prompt("Enter the Radius of a circle");
+//radius = Number (radius);
+//circumference = 2 * PI * radius;
+//document.getElementById("PiCount").textContent = circumference;
+//console.log(circumference);
