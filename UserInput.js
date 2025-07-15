9e8ba53667f75ba29fb2184be24fe7c468beeca8
Sapi <naveedkhansapi@gmail.com>
2025-07-15 11:20:13 +0200

    Type Converstion added into page

--- UserInput.js
+++ UserInput.js
@@ -4,6 +4,7 @@
 // 1. Easy way user promt
 //2. professional way=Html TextBox
 
+/*
 
 let username;
 username=window.prompt("What is your name");
@@ -17,3 +18,27 @@ document.getElementById("btnsubmit").onclick = function(){
    document.getElementById("txtboxDataDisplay").textContent= `Your Full Name is : ${username1}`;
     //console.log(username1);
 }
+*/
+
+//Type Converstion= change the datatype of a another 
+//                         (strings, number, booleans)
+// 1 .Example
+
+let age=window.prompt("how old are you");
+age=Number(age);
+age +=1;
+console.log(age);
+console.log(age, typeof age);
+
+let x = "";
+let y = "";
+let z = "";
+
+x = Number (x);
+y = String (y);
+z = Boolean (z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
