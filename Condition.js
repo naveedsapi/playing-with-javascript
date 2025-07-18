48eaf1af2f63f26ac0ac84bcfe21fe3b8dce6bbc
Sapi <naveedkhansapi@gmail.com>
2025-07-18 10:19:37 +0200

    Conditon JavaScript file added

---
+++ Condition.js
@@ -0,0 +1,11 @@
+
+
+
+let age = window.prompt("Enter your Age");
+
+if(age >= 18){
+    document.getElementById("labage").textContent ="You are  "+ age+" Years old";
+   
+}else{
+    document.getElementById("labage").textContent="You are Under Age "+age+" Years Old";
+}
