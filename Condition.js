168e2e938f5c5cc1705579e92abec019108a4b39
Sapi <naveedkhansapi@gmail.com>
2025-07-19 23:50:43 +0200

    Tacking age Form Text Promt

--- Condition.js
+++ Condition.js
@@ -1,6 +1,6 @@
 
 
-
+/*
 let age = window.prompt("Enter your Age");
 
 if(age >= 18){
@@ -9,3 +9,26 @@ if(age >= 18){
 }else{
     document.getElementById("labage").textContent="You are Under Age "+age+" Years Old";
 }
+    */
+   let agelabel= document.getElementById("agelabel");
+   let txtage = document.getElementById("txtage");
+   let buttonage = document.getElementById("btnage");
+    let age;
+
+   buttonage.onclick = function(){
+       age = txtage.value;
+       age = Number(age);
+      if(age<=18){
+        agelabel.textContent="Your age not allowed To Enter This sit"+age+"Under age";
+       
+      }else if(age>=18 && age<=30){
+        agelabel.textContent="You are "+age+" years old";
+      }else if(age>=30 && age<=50){
+         agelabel.textContent="you age is 30 between 50 your old";
+
+      }else {
+        agelabel.textContent="Welcom to Our site";
+      }
+   }
+
+   
