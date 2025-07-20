aa2aaa02a9784bc314bd7b7de512e5b521f2dada
Sapi <naveedkhansapi@gmail.com>
2025-07-20 03:57:20 +0200

    JavaScript file Added into page

---
+++ Condition1.js
@@ -0,0 +1,8 @@
+
+const checkBtn=document.getElementById("btncheck");
+const visaBtn=document.getElementById("btnVisa");
+const  masterBtn=document.getElementById("btnMaster");
+const paypalBtn=document.getElementById("btnPay");
+
+ document.getElementById("Submit");
+ 
