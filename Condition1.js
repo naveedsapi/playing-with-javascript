e8d002e8a627250ca38db1e9c2a2fe01fdc88188
Sapi <naveedkhansapi@gmail.com>
2025-07-20 05:24:20 +0200

    Programe has been Done by If Condition

--- Condition1.js
+++ Condition1.js
@@ -1,8 +1,29 @@
 
-const checkBtn=document.getElementById("btncheck");
-const visaBtn=document.getElementById("btnVisa");
-const  masterBtn=document.getElementById("btnMaster");
-const paypalBtn=document.getElementById("btnPay");
+const checkBox = document.getElementById("btncheck");
+const visaBtn = document.getElementById("btnVisa");
+const  masterBtn = document.getElementById("btnMaster");
+const paypalBtn = document.getElementById("btnPay");
+const subResult = document.getElementById("subresult");
+const paymentResult = document.getElementById("paymentResult");
 
- document.getElementById("Submit");
- 
+const submitBtn = document.getElementById("Submit");
+
+ submitBtn.onclick = function(){
+
+    if(checkBox.checked){
+     subResult.textContent = "You are Subscrib";
+    }else{
+        subResult.textContent="You are not Subscrib";
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent="You are paying with Visa";
+    }else if(masterBtn.checked){
+        paymentResult.textContent="You are paying with Master Card";
+    }else if(paypalBtn.checked){
+              paymentResult.textContent="You are paying with Paypal";
+    }else {
+        paymentResult.textContent="You musst Select a Payment Type";
+    }
+}
+
