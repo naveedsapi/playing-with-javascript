91d88014984c91a20d89d9f3675996cf0b7422c7
Sapi <naveedkhansapi@gmail.com>
2025-07-21 11:35:07 +0200

    changes in js file

--- Switch.js
+++ Switch.js
@@ -2,11 +2,11 @@
 
 
 let txtday = document.getElementById("txtday");
-let day= txtday.value;
+//let day= txtday.value;
 let txtmonth = document.getElementById("txtmonth");
-let month=txtmonth.value;
+//let month=txtmonth.value;
 let txtcountry = document.getElementById("txtcountry");
-let country=txtcountry.value;
+//let country=txtcountry.value;
 
 let labelday = document.getElementById("labelday");
 let labelmonth = document.getElementById("labelmonth");
@@ -18,45 +18,80 @@ let btncountry = document.getElementById("btncountry");
 
 
 btnday.onclick = function(){
-
-    switch(day){
-
-          case day = 1 :
-           labelday.textContent="Monday";
-          break;
-          case day = 2 :
-            labelday.textContent="Tuesday";
-            break;
-              case day = 3 :
+      
+    let day= txtday.value;
+         if(day==1){
+            labelday.textContent="Monday";
+         }else if(day==2) {
+          labelday.textContent="Tuesday";
+        }else if(day==3){
             labelday.textContent="Wensday";
-            break;
-            default:
-                {
-                    labelday.textContent="Please Enter The Day of Week!";
-                }
-    }
+        }else if(day==4) {
+          labelday.textContent="Thursday";
+        }else if(day==5){
+            labelday.textContent="Friday";
+         }else if(day==6) {
+          labelday.textContent="Saterday";
+        }else if(day==7){ 
+          labelday.textContent="Sunday";
+        }
+        else {
+            labelday.textContent="Please Enter The Day of Week!";
+        }       
+                       
+    
 }
 
 
 btnmonth.onclick = function(){
 
-    switch(month){
-
-        case month = 1:
-        labelmonth.textContent="Janvery";
-        break;
-        case month = 2:
-            labelmonth.textContent="Febervery";
-            break;
-            default:
-                {
-                    labelmonth.textContent="Please Enter the Month";
-                }
-    }
+     let month= txtmonth.value;
+         if(month==1){
+            labelmonth.textContent="Jan";
+         }else if(month==2) {
+          labelmonth.textContent="Feb";
+        }else if(month==3){
+            labelmonth.textContent="Mar";
+        }else if(month==4) {
+          labelmonth.textContent="Apr";
+        }else if(month==5){
+            labelmonth.textContent="May";
+         }else if(month==6) {
+          labelmonth.textContent="Jun";
+        }else if(month==7){ 
+          labelmonth.textContent="July";
+        }else if(month==8){ 
+          labelmonth.textContent="Aug";
+        }else if(month==9){ 
+          labelmonth.textContent="Sep";
+        }else if(month==10){ 
+          labelmonth.textContent="Okt";
+        }else if(month==11){ 
+          labelmonth.textContent="Nov";
+        }else if(month==12){ 
+          labelmonth.textContent="Dec";
+        }
+        else {
+            labelmonth.textContent="Please Enter The Day of Month!";
+        }       
+                   
 }
 
 
 btncountry.onclick = function(){
+
+       let country=txtcountry.value;
+    if(country==49) {
+        labelcountry.textContent = "Deutschland";
+    }else if(country==42){
+        labelcountry.textContent="United Stats of Amireka";
+    }else if(country==41){
+        labelcountry.textContent="India";
+    }else{
+         labelcountry.textContent=" The Country Code Dose not Exist";
+    
+        }
+/*
 
     switch(country){
 
@@ -71,4 +106,5 @@ btncountry.onclick = function(){
                     labelcountry.textContent=" The Country Code Dose not Exist";
                 }
     }
+                */
 }
