a0964f58ec300ef7ee695bf2b68a3d80a23ea518
Sapi <naveedkhansapi@gmail.com>
2025-08-03 05:31:57 +0200

    Function js file added

---
+++ Function.js
@@ -0,0 +1,15 @@
+
+
+let vorname;
+let age;
+let mylocation;
+function hallo(vorname,age,mylocation){
+    console.log("Hallo World my Name is "+ vorname+"age is "+age+"address"+mylocation);
+    console.log("You are Beautiful and my Name is  "+ vorname+"age is "+age+"address"+mylocation);
+    console.log("I Love You my Beautiful World and my Name is  "+ vorname+"age is "+age+"address"+mylocation);
+}
+
+
+hallo("Miguel","22","Datteln");
+hallo("Miguel","22","Datteln");
+hallo("Miguel","22","Datteln");
